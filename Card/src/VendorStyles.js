bdc4c2f6a2d0db2d5bc1c03efd601df8cbd5fee4
khemarawestman <erikkhemarawestman@hotmail.se>
2024-04-11 22:02:03 +0200

    push

--- Card/src/VendorStyles.js
+++ Card/src/VendorStyles.js
@@ -1,22 +1,20 @@
-// src/utils/VendorStyles.js
 import DefaultLogo from '../src/assets/vendor-blockchain.svg';
 import Vendor1Logo from '../src/assets/vendor-bitcoin.svg';
 import Vendor2Logo from '../src/assets/vendor-blockchain.svg';
-// Import other vendor logos
 
 const VendorStyles = {
   vendor1: {
     logo: Vendor1Logo,
-    color: "#FF4500", // Example color for Vendor 1
+    color: "#FF4500", 
   },
   vendor2: {
     logo: Vendor2Logo,
-    color: "#1E90FF", // Example color for Vendor 2
+    color: "#1E90FF", 
   },
-  // Add more vendors as needed
+
   default: {
     logo: DefaultLogo,
-    color: "#FFFFFF", // Default color
+    color: "#FFFFFF", 
   },
 };
 
