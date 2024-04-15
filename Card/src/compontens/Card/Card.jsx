6ecdb728023dc5ea40746407b358103482f931be
khemarawestman <erikkhemarawestman@hotmail.se>
2024-04-15 21:08:09 +0200

    push

--- Card/src/compontens/Card/Card.jsx
+++ Card/src/compontens/Card/Card.jsx
@@ -12,10 +12,11 @@ const Card = ({ cardNumber, cardHolderName, validThru, vendor }) => {
     <div className="card" style={{ backgroundColor: color }}>
       <div className="card-logo-section">
         <img src={logo} alt="Vendor" className="card-vendor-logo" />
+        <div className="preview-chip-icon"></div>
       </div>
       <div className="card-number">{formattedCardNumber || 'XXXX XXXX XXXX XXXX'}</div>
-      <div className="card-holder">{cardHolderName.toUpperCase() || 'FULL NAME'}</div>
-      <div className="card-expiry">
+      <div className="card-holder">CARDHOLDER NAME<br></br>{cardHolderName.toUpperCase() || 'FULL NAME'}</div>
+      <div className="card-expiry">VALID THRU<br></br>
 {validThru || 'MM/YY'}</div>
     </div>
   );
